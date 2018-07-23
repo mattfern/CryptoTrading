1df3b9fb332b916e9efdc1c22348478602ab03d6
mattfern <mattfern2653@gmail.com>
2018-07-23 21:07:24 +1000

    Binance Symbol Complete

---
+++ Binance_Listings.py
@@ -0,0 +1,24 @@
+import csv
+import json
+import urllib.request
+import pandas as pd
+
+# Get Symbols from binance api
+def get_symbols():
+    url = "https://www.binance.com/api/v1/exchangeInfo"
+
+    data = json.load(urllib.request.urlopen(url))
+
+    symbols = data['symbols']
+
+    with open('Binance_Symbols.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Symbol']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for s in symbols:
+            symb = s['symbol']
+            writer.writerow({'Symbol': symb})
+            print(s['symbol'])
+
+get_symbols()
